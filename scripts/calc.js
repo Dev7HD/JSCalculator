b8e2db361a466e1cfbbc1265f52a5d5052bf5746
Dev7HD <hamza.damiri@gmail.com>
2023-12-14 22:37:41 +0100

    code improvement, bugs fix and new feature added. Add popup on clicking on 'AC'

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,3 @@
-// Logging information about the calculator
 console.log("Javascript Calculator Made by Dev7HD\nhttps://github.com/Dev7HD");
 
 // Variable declarations
@@ -7,14 +6,12 @@ let storArr = [];
 let screenValue = '';
 let flag = 0;
 let isSign = true;
-const errorModal = document.getElementById("errorModal");
 
 // Function to check if a character is a number
 const isNumber = (char) => /^\d$/.test(char);
 
 // Getting the display element
-const screen = document.getElementById("answer");
-screen.readOnly = true;
+const $screen = $("#answer").prop("readOnly", true);
 
 // Function to handle button click
 const handleButtonClick = (buttonText) => {
@@ -22,72 +19,66 @@ const handleButtonClick = (buttonText) => {
   if (buttonText === "X" && !isSign) {
     buttonText = "*";
     screenValue += buttonText;
+    isSign = true;
   } else if (buttonText === "AC") {
     eraseInput();
-    $("#answer").focus()
+    // Add pop-up on click on 'AC'
+    $('.overlay').addClass('show');
+    $screen.focus();
   } else if (buttonText === "=") {
     makeCal();
   } else if (buttonText === "(" || buttonText === ")") {
     screenValue += buttonText;
     op = screenValue;
   } else if (isNumber(buttonText)) {
-    if (flag === 1) {
-      screenValue = buttonText;
-      op = screenValue;
-      flag = 0;
-    } else {
-      screenValue += buttonText;
-      op = screenValue;
-    }
+    screenValue = flag ? buttonText : screenValue + buttonText;
+    op = screenValue;
+    flag = 0;
     isSign = false;
-    screen.classList.remove("negative");
+    $screen.removeClass("negative");
   } else {
     if (!isSign) {
-      screenValue = screen.value + buttonText;
+      screenValue = $screen.val() + buttonText;
       isSign = true;
       op = screenValue;
     }
-    screen.classList.remove("negative");
+    $screen.removeClass("negative");
   }
-  screen.value = screenValue;
+  $screen.val(screenValue);
 };
 
 // Adding click event listeners to buttons
-document.querySelectorAll("button").forEach((item) => {
-  item.addEventListener("click", (e) => {
-    const buttonText = e.target.innerText;
-    handleButtonClick(buttonText);
-  });
+$("button").on("click", function () {
+  const buttonText = $(this).text();
+  handleButtonClick(buttonText);
 });
 
 // Handling keyboard inputs
-$(document).keydown(function (event) {
+$(document).on("keydown", function (event) {
   const key = event.key;
   if (key === 'Enter') {
-    op = screen.value;
+    op = $screen.val();
     makeCal();
   } else if (key === 'Escape') {
     //Handling 'Escape' keydown event
-    errorModal.classList.remove("show")
-    $('#history').removeClass("show")
-    $('#bar1').hide(500)
-    $('#bar2').hide(500)
+    $('#errorModal, #history').removeClass("show");
+    $('#bar1, #bar2').hide(500);
     eraseInput();
+    $('.overlay').removeClass('show');
   } else if (!isNaN(key) || '+-*/.()'.includes(key)) {
-    screenValue = screen.value + key;
-    screen.value = screenValue;
+    screenValue += key;
+    $screen.val(screenValue);
   } else if (key === 'Backspace') {
-    screenValue = screen.value.slice(0, -1);
-    screen.value = screenValue;
-    screen.classList.toggle("negative", parseFloat(screenValue) < 0);
+    screenValue = $screen.val().slice(0, -1);
+    $screen.val(screenValue);
+    $screen.toggleClass("negative", parseFloat(screenValue) < 0);
   }
 });
 
 // Function to perform calculations
 const makeCal = () => {
-  // Check for brackets and perform calculations
   checkForBracketMulti();
-  screen.classList.toggle("negative", parseFloat(screen.value) < 0);
+  $screen.toggleClass("negative", parseFloat($screen.val()) < 0);
 };
 
 // Function to erase input
@@ -95,14 +86,13 @@ const eraseInput = () => {
   // Clearing input values and removing classes
   op = '';
   screenValue = '';
-  screen.value = screenValue;
-  screen.classList.remove("negative");
+  $screen.val(screenValue);
+  $screen.removeClass("negative");
   isSign = true;
 };
 
 // Function to store calculation history
 const storing = (operation, result) => {
-  // Storing operations and results
   storArr.push({ op: operation, result: result });
   localStorage.setItem("calcHistory", JSON.stringify(storArr));
 };
@@ -110,29 +100,29 @@ const storing = (operation, result) => {
 // Function to check for bracket multiplication
 function checkForBracketMulti() {
   // Checking and performing calculations involving brackets
-  const calculate = new Function('return ' + screenValue);
-  const calculateValue = calculate();
+  const calculateValue = new Function('return ' + screenValue)();
   if (calculateValue !== undefined) {
-    screen.value = calculateValue;
-    screenValue = screen.value;
+    $screen.val(calculateValue);
+    screenValue = $screen.val();
     let res = screenValue;
     storing(op, res);
-    screen.classList.toggle("negative", parseFloat(screen.value) < 0);
+    $screen.toggleClass("negative", parseFloat($screen.val()) < 0);
   }
   flag = 1;
 }
 
 // Error handling
-
-window.onerror =  () => {
-  const errorMessage = document.getElementById("errorMessage");
-  errorMessage.textContent = "PLEASE INPUT VALID EXPRESSION";
-  errorModal.classList.add("show")
+window.onerror = () => {
+  $('#errorModal').addClass("show");
   // Prevent default browser error handling
   return false;
 };
 
 // Close the modal when clicking the close button (×)
-document.querySelector(".close").addEventListener("click", function () {
-  errorModal.classList.remove("show")
+$('#errorModal .modal-content .close').on("click", () => {
+  $('#errorModal').removeClass("show");
+});
+
+$('#popup1 .popup_1 .close').on('click', () => {
+  $('.overlay').removeClass('show');
 });
